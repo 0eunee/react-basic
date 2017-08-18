047a1e133a934261cfded486c38ebd0cd07dacfb
youngeun <0eunee0@gmail.com>
2017-08-18 10:26:50 +0900

    Mod index.js in store folder
    로그 기록

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, compose, applyMiddleware } from "redux";
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import thunk from "redux-thunk";
 
 export default function configureStore(reducer, initialState = {}) {
+    const logger = createLogger();
     const enhancer = compose(
-        applyMiddleware(thunk/* , middleware 추가*/)
+        applyMiddleware(thunk,promise, logger/* , middleware 추가*/)
     );
-
     return createStore(reducer, initialState, enhancer);
 }
